ad48cb487ca30a268d5ad14320ddacd02325e1e5
Konrad Mösch <konrad@moesch.org>
2022-07-11 00:26:54 +0200

    Add correct solution for project 1

--- main.R
+++ main.R
@@ -2,10 +2,7 @@
 random_numbers <- rnorm(10000, 0, 1)
 
 # a
-hist(random_numbers, freq = FALSE, xlim = c(-4, 4))
-x <- c(-4, -3, -2, -1, 0, 1, 2, 3, 4)
-d <- dnorm(x)
-hist(d, col = 'blue', freq = FALSE, add = TRUE)
+hist(random_numbers, col = "blue", freq = FALSE); plot(dnorm, col = 'red', xlim = c(-4, 4), add = TRUE)
 
 # b
 rel_freq <- function(x, v) {
@@ -26,9 +23,7 @@ for (x in x_values) {
   y_values[index]= rel_freq(x, random_numbers)
   index = index + 1
 }
-plot(x_values, y_values)
-plot(x_values, pnorm(x_values))
-#distribution_function <- pnorm(random_numbers)
+plot(x_values, 1/10000*y_values, col = "red"); plot(pnorm, xlim = c(-4,4), add = TRUE)
 
 
 # Problem 2
@@ -37,16 +32,20 @@ poisson_numbers <- rpois(10000, lambda)
 
 # a
 pois_mean <- mean(poisson_numbers)
+expected <- lambda
 # expected mean: E[X]=lambda
-difference <- abs(pois_mean - lambda)
+difference <- abs(pois_mean - expected)
 
 # b
 squared <- poisson_numbers^2
 squared_mean <- mean(squared)
+expected <- lambda^2 + lambda
 # expected: squared_mean = mean^2 = lambda^2;
-difference2 <- abs(squared_mean - lambda^2)
+difference2 <- abs(squared_mean - expected)
 
 # c
 # show that E[lambda^-X]=e^(1-lambda)?
 # Here: E[X^2] (b) -> lambda=X;X=-2?
-exp(1 + 2)
+#exp(1 + 2)
+calculated_expected_value <- mean(lambda^(-poisson_numbers))
+difference3 <- calculated_expected_value - exp(1-lambda)
