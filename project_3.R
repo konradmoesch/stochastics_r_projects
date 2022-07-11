e4238225f3c5f3b3fa6567b2e2121da3e3f3896d
Konrad Mösch <konrad@moesch.org>
2022-07-12 00:32:49 +0200

    Finish project_3?

--- project_3.R
+++ project_3.R
@@ -49,16 +49,35 @@ library(compositions)
 
 # ToDo:
 # 2) Hypotheses:
+#  H0: mu = 171
+#  H1: mu != 171
 # 3) Error level:
+#  alpha = 0.95
 # 4) Test statistic:
+#  Two-Sided Gauss test
 # 5) Perform the test:
+Gauss.test(unlist(height), mean = 171, sd = 11, alternative = "two.sided")
 
 # 6) Decision:
-
+#  T = 171.38
+#  p-value: 0.001679 < 0.05 ?
+# TODO: verify correctness
+#  the avg value does not differ from 171, the test proves that with an error
+#  level smaller than alpha.
 
 # ToDo: 2d)
+height <- unlist(height)
+error_margin <- qt(0.95, df = length(height)-1)*11/sqrt(length(height))
+lower_bound <- mean(height) - error_margin
+upper_bound <- mean(height) + error_margin
+print(paste("Interval: [", lower_bound, ", ", upper_bound, "]"))
 # Does this interval fit to your result in c)?
+#  Yes, the calculated T-value is inside the confidence interval??
+# TODO: confirm correctness of this sentence and calculations / formulas
 
 # ToDo: 2e)
 # Throughout the task we assumed that the height is normally distributed.
 # Is this assumption reasonable in your opinion?
+# Yes, we can simplify the height of humans as normally distributed;
+#  there are many people of some "middle" height and a few people that
+#  are bigger or smaller than that.
